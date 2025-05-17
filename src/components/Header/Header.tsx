1219c0cc0d27e33900a9a75eb195d9e58045ed44
caiocesartec <newcesar25@gmail.com>
2025-05-17 19:00:23 -0300

    menuMob

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,46 @@
+'use client';
+
 import styles from './Header.module.css';
 import HeaderOptions from '@/components/HeaderOptions/HeaderOptions';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
-    return (
-        <header className={styles.headerMain}>
-            {/* opções do menu */}
-            <section className={styles.containerOptionsMenu}>
-                <HeaderOptions />
-            </section>
-        </header>
-    );
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkResolution = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkResolution();
+    window.addEventListener('resize', checkResolution);
+
+    return () => {
+      window.removeEventListener('resize', checkResolution);
+    };
+  }, []);
+
+  const deskOrMobContainer = `${styles.containerOptionsMenu} ${isMobile ? styles.containerOptionsMenuMobile : ''}`;
+
+  return (
+    <header className={styles.headerMain}>
+      {/* opções do menu */}
+      <section className={deskOrMobContainer}>
+        {isMobile && (
+          <nav>
+            <ul>
+              <li className={styles.mobileMenuIcon}>
+                <a>
+                  <FontAwesomeIcon icon={faBars} />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        )}
+        <HeaderOptions />
+      </section>
+    </header>
+  );
 }
--- src/components/HeaderOptions/HeaderOptions.tsx
+++ src/components/HeaderOptions/HeaderOptions.tsx
@@ -1,23 +1,20 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBars } from '@fortawesome/free-solid-svg-icons';
-
 import Link from 'next/link';
 import { navOptions } from '@/data/navOptions';
 
 const Menu = () => {
-    return (
-        <nav>
-            <ul>
-                {navOptions.map((option) => (
-                    <li key={option.path}>
-                        <Link key={option.path} href={option.path}>
-                            {option.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        {navOptions.map((option) => (
+          <li key={option.path}>
+            <Link key={option.path} href={option.path}>
+              {option.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
