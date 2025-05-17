4d861fa25337fdb9818b0a286c0db298cb94142c
caiocesartec <newcesar25@gmail.com>
2025-05-17 18:30:24 -0300

    headerMob-init

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
-import styles from "./Header.module.css"
-import HeaderOptions from "@/components/HeaderOptions/HeaderOptions"
+import styles from './Header.module.css';
+import HeaderOptions from '@/components/HeaderOptions/HeaderOptions';
 
 export default function Header() {
     return (
-        <header className={styles.headerMain}>           
+        <header className={styles.headerMain}>
             {/* opções do menu */}
             <section className={styles.containerOptionsMenu}>
-                <nav> 
-                    <ul>
-                        <HeaderOptions/>
-                    </ul>
-                </nav>
+                <HeaderOptions />
             </section>
         </header>
-    )
+    );
 }
--- src/components/HeaderOptions/HeaderOptions.tsx
+++ src/components/HeaderOptions/HeaderOptions.tsx
@@ -1,21 +1,23 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-import Link from "next/link";
-import { navOptions } from "@/data/navOptions";
+import Link from 'next/link';
+import { navOptions } from '@/data/navOptions';
 
 const Menu = () => {
-	return (
-		<>
-			{navOptions.map((option) => (
-				<li key={option.path} >
-					<Link key={option.path} href={option.path}>
-						{option.label}
-					</Link>
-				</li>
-			))}
-		</>
-	);
+    return (
+        <nav>
+            <ul>
+                {navOptions.map((option) => (
+                    <li key={option.path}>
+                        <Link key={option.path} href={option.path}>
+                            {option.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 };
 
 export default Menu;
